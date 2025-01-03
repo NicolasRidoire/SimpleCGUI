dc27401aabb834f4ebe76024a5671be28d5bacf9
NicolasRidoire <nridoire@gmail.com>
2025-01-03 20:08:02 +0100

    Fix Windows not having window controls

--- main.c
+++ main.c
@@ -7,6 +7,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     ERRORS err = initWindow(windowClassName, hInstance);
     if (err != OK) 
         return err;
+    err = createWindow("NES Emulator",
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT, CW_USEDEFAULT, 800, 600);
+    if (err != OK)
+        return err;
     return demo();
 }
 #elif __linux__
@@ -14,16 +19,14 @@ int main() {
     ERRORS err = initWindow(NULL, NULL);
     if (err != OK) 
         return err;
-    return demo();
-}
-#endif
-int demo() {
-    ERRORS err;
-
     err = createWindow("NES Emulator", 0, 0, 0, 800, 600);
     if (err != OK)
         return err;
 
+    return demo();
+}
+#endif
+int demo() {
     Color rectColor = {
         .red = 0,
         .green = 255,
@@ -39,7 +42,7 @@ int demo() {
     editRectangle(rectId2, -1, -1, 200, 200, rectColor2);
     deleteRectangle(rectId1);
 
-    err = mainLoop();
+    ERRORS err = mainLoop();
 
     printf("Finished with error code %d\n", err);
     return err;
